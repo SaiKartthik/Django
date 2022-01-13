bce4573b08276f76d8a42b76ccbcc9b3f05c927c
SaiKartthik <saikarthikmedepalli@gmail.com>
2022-01-13 22:25:04 +0530

    Adding two numbers

--- calc/views.py
+++ calc/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+
 
 def home(request):
-    return render(request, 'home.html')
-# Create your views here.
+    return render(request, 'home.html', {'name':"Sai"})
+
+# @csrf_exempt
+def add(request):
+    # val1 = request.GET['num1']
+    # val2 = request.GET['num2']
+    # val1 = request.GET['num1']
+    val1 =int(request.POST.get('num1'))
+    val2 =int(request.POST.get('num2'))
+    res = val1+val2
+    return render(request, 'result.html', {'result' :res})
+
+#
